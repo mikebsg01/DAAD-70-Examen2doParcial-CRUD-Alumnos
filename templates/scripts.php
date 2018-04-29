5ac219023fbe748f5e27b9a736dfb3484fb92c04
Michael Serrato <mikebsg01@gmail.com>
2018-04-29 18:32:33 -0500

    Compile Assets for Production Mode.

--- templates/scripts.php
+++ templates/scripts.php
@@ -1,3 +1,3 @@
 <script type="text/javascript" src="assets/js/jquery-3.2.1.min.js"></script>
 <script type="text/javascript" src="assets/js/materialize.min.js"></script>
-<script type="text/javascript" src="assets/js/app.js?v=<?php echo time() ?>"></script>
+<script type="text/javascript" src="assets/js/app.js"></script>
